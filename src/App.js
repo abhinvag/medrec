eb4320ed01043f1cc2a55ad70b2c680371860a2a
wraith-0 <19ucs254@lnmiit.ac.in>
2022-05-13 12:26:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Web3 from "web3";
 
 function App() {
+  const [account, setAccount] = useState("");
+
+  useEffect(() => {
+    const fetchAccount = async () => {
+      const web3 = new Web3("http://127.0.0.1:8545");
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+
+    fetchAccount();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{account}</div>
     </div>
   );
 }
